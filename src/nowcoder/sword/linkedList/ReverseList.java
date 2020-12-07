d0de64f07ecb448350397e39d21e1cd34da31048
honghaoteng <>
2020-12-07 11:19:17 +0800

    剑指offer-20201207-反转链表

--- src/nowcoder/sword/linkedList/ReverseList.java
+++ src/nowcoder/sword/linkedList/ReverseList.java
@@ -10,4 +10,27 @@ public class ReverseList {
      * 输入一个链表，反转链表后，输出新链表的表头。
      */
 
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode preNode = null;
+        ListNode rHead = null;
+        ListNode curNode = head;
+        while (curNode != null) {
+
+            ListNode nextNode = curNode.next;
+            if (nextNode == null) {
+                rHead = curNode;
+            }
+
+            curNode.next = preNode;
+            //重新赋值
+            preNode = curNode;
+            curNode = nextNode;
+
+        }
+        return rHead;
+    }
+
 }
